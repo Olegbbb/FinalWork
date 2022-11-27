f2f56e1b69bc1061deb0beaeacbc7a886957fdf1
Oleg <apricot-ob@yandex.ru>
2022-11-28 00:07:15 +0300

    Второй вариант

---
+++ work2/Program.cs
@@ -0,0 +1,30 @@
+﻿void CreateArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        string? a = Convert.ToString(Console.ReadLine());
+        array[i] = a!;
+    }
+}
+void SecondArray(string[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            Console.Write(array[i] + ", ");
+        }
+    }
+    Console.Write("]");
+}
+
+Console.WriteLine("Введите количество элементов в массиве");
+int n = Convert.ToInt32(Console.ReadLine());
+string[] array = new string[n];
+Console.WriteLine("Введите элемент массива");
+CreateArray(array);
+Console.WriteLine("Первоначальный массив: ");
+Console.WriteLine("[" + string.Join(", ", array) + "] ");
+Console.WriteLine("Искомый массив: ");
+SecondArray(array);
